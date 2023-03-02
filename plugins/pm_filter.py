7784c90938f2de48fad24b8d35a5974c792eddf4
Raj Kirat Singh <93331120+Itsraj3600@users.noreply.github.com>
2023-03-02 17:42:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -61,7 +61,7 @@ async def pm_text(bot, message):
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
     if user_id in ADMINS: return # ignore admins
-    await message.reply_text("<b>YEH MOVIE APKO YAHA MILEGI 👉 YAHA PAR CONTACT  ----> [ @Moviescrownhelpbot ] !</b>")
+    await message.reply_text("<b>SEND U R MESSAGE HERE 👉👉   ----> [ @Priyanka_samrottbot ] / GROUP HERE 👉👉 @moviescrown !</b>")
     await bot.send_message(
         chat_id=LOG_CHANNEL,
         text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
