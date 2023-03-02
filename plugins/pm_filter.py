8bbfc9752f7d1a01a2cf9db27df9a59ff255b8c2
Raj Kirat Singh <93331120+Itsraj3600@users.noreply.github.com>
2023-03-02 17:22:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -269,8 +269,7 @@ async def next_page(bot, query):
                     ],
                 )
     btn.insert(0, [
-        InlineKeyboardButton("⚡ H̳O̳W̳ ̳T̳O̳ ̳D̳O̳W̳N̳L̳O̳A̳D̳ ⚡", url=f"https://t.me/how_to_download_movie_from_bot"),
-        InlineKeyboardButton("🙏 ADMIN 🌹", url=f"https://t.me/Moviescrownhelpbot")
+        InlineKeyboardButton("H̳O̳W̳ ̳T̳O̳ ̳D̳O̳W̳N̳L̳O̳A̳D̳", url=f"https://t.me/how_to_download_movie_from_bot")
     ])
     try:
         await query.edit_message_reply_markup(
@@ -1410,8 +1409,7 @@ async def auto_filter(client, msg, spoll=False):
             )
 
     btn.insert(0, [
-        InlineKeyboardButton("⚡ H̳O̳W̳ ̳T̳O̳ ̳D̳O̳W̳N̳L̳O̳A̳D̳ ⚡", url=f"https://t.me/how_to_download_movie_from_bot"),
-        InlineKeyboardButton("🙏 ADMIN 🌹", url=f"https://t.me/Moviescrownhelpbot")
+        InlineKeyboardButton("H̳O̳W̳ ̳T̳O̳ ̳D̳O̳W̳N̳L̳O̳A̳D̳", url=f"https://t.me/how_to_download_movie_from_bot")
     ])
 
     if offset != "":
